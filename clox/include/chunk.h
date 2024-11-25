a82f5dce7944453586b30d493cb2fe880d0bed81
jabberwocky238 <jabberwocky238@gmail.com>
2024-11-25 14:17:08 +0800

    clox: 15.3

--- clox/include/chunk.h
+++ clox/include/chunk.h
@@ -8,6 +8,11 @@
 typedef enum
 {
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
@@ -16,15 +21,14 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
-    int* lines;
+    int *lines;
     ValueArray constants;
 } Chunk;
 
-void initChunk(Chunk* chunk);
+void initChunk(Chunk *chunk);
 // void writeChunk(Chunk* chunk, uint8_t byte);
-void writeChunk(Chunk* chunk, uint8_t byte, int line);
-int addConstant(Chunk* chunk, Value value);
-void freeChunk(Chunk* chunk);
-
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+int addConstant(Chunk *chunk, Value value);
+void freeChunk(Chunk *chunk);
 
 #endif
