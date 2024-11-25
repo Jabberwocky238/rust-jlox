564d7eb02c091ca12e8fd6b914056325c1cde2ee
jabberwocky238 <jabberwocky238@gmail.com>
2024-11-25 15:05:52 +0800

    clox: ready for 17.1

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -97,12 +97,20 @@ static InterpretResult run()
 
 InterpretResult interpret(const char *source)
 {
-    compile(source);
-    return INTERPRET_OK;
-}
-InterpretResult interpret(Chunk *chunk)
-{
-    vm.chunk = chunk;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk))
+    {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
     vm.ip = vm.chunk->code;
-    return run();
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
