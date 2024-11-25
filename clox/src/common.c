33ced7dada0c7f04369fe1f13a462f824e22c9e9
jabberwocky238 <jabberwocky238@gmail.com>
2024-11-25 14:07:32 +0800

    clox: 15.2

--- clox/src/common.c
+++ clox/src/common.c
@@ -1,16 +1,19 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
+
+#define DEBUG_TRACE_EXECUTION
 
 int main(int argc, const char *argv[])
 {
   Chunk chunk;
   initChunk(&chunk);
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_RETURN, 123);
 
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, constant);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
   
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
